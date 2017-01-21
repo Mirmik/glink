1913311a18e0dbe3b215f3b01baa6e6638bc978d
Mirmik <mirmikns@yandex.ru>
2017-01-21 20:53:46 +0300

    a

--- templates/init/glink.lua
+++ templates/init/glink.lua
@@ -1,1 +1,21 @@
-dadas
+ruller = CXXDeclarativeRuller.new{
+	buildutils = { 
+		CXX = "g++", 
+		CC = "gcc", 
+		LD = "ld", 
+	},
+	builddir = "./build",
+}
+
+Module("main", {
+	sources = {
+	},
+})
+
+local ret = ruller:standartAssemble("main", {
+	target = "target",
+	targetdir = "./",
+	assembletype = "application"
+})
+
+if not ret then print(text.yellow("Nothing to do")) end
